664848581bcb7f3ceb3e2b674b33d9bfad70f8a9
KEVIN <zjh459@gmail.com>
2014-01-06 17:19:05 +0800

    init 1

---
+++ src/main/java/com/sample/mvc/HelloController.java
@@ -0,0 +1,16 @@
+package com.sample.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Simple Git Test!");
+		return "hello";
+	}
+}
